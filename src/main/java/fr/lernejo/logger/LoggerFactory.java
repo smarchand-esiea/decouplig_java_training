ccc072812e21a825547174060c1de2a20d2fe61a
smarchand-esiea <smarchand@et.esiea.fr>
2021-12-06 16:03:42 +0100

    Add FilteredLogger Partie 7 fin TP

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -1,12 +1,16 @@
 package fr.lernejo.logger;
 
+import java.util.function.Predicate;
+
 public class LoggerFactory {
 
     public static Logger getLogger(String name)
     {
-
+        Predicate<String> condition = message -> !message.contains("player");
         return new CompositeLogger(new ContextualLogger(name,new ConsoleLogger())
-            ,new ContextualLogger(name, new FileLogger("./Log.txt"))
+            ,new ContextualLogger(name,new FilteredLogger(
+                new FileLogger("./Log.txt"), condition)
+            )
         );
     }
 }
